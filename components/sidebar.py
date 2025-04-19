ac8f3ca2b7eab919e8b9b92725248c6dd21ddb9b
Henry <henryzhiye@gmail.com>
2025-04-19 14:54:45 +1000

    removed streamlit files

--- components/sidebar.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def render_sidebar():
-    st.sidebar.header("Home")
--- utils/data_utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def merge_suburb(df_suburbs, df_prop, state):
-    suburbs = df_suburbs["name"][df_suburbs["state"] == state].unique()
-    df_filtered = df_prop[df_prop["Suburb"].isin(suburbs)].merge(df_suburbs, left_on='Suburb', right_on='name')
-    return df_filtered
